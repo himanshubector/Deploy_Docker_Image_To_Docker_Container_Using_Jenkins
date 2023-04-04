d4bce9bc2cd536a842c49ab24d5d189b079e4bc7
himanshubector <smk.131@gmail.com>
2023-04-04 22:46:56 +0200

    first commit

---
+++ src/main/java/com/himanshubeci/devops/integration/DevopsIntegrationApplication.java
@@ -0,0 +1,22 @@
+package com.himanshubeci.devops.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication
+{
+	@GetMapping
+	public String message()
+	{
+		return "Welcome to Docker Jenkins Pipeline integration !!!!!!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+}
